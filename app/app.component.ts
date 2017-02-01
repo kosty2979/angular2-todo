31a3388ea39f27bad2b5238d21f5f1a9fc2b9837
kosty2979 <kosty2979@gmail.com>
2017-02-01 11:13:29 +0200

    decomposition

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,24 +1,9 @@
 import { Component } from '@angular/core';
 
-class Todo{
-	constructor(public title: string,
-				  public completed: boolean = false){}
-}
+import { Todo } from './shared/todo';
+import { todos } from './shared/data'
 
-const todos: Todo[]=[
-	{
-		title:"изучить js",
-		completed: true
-	},
-	{
-		title:"изучить node",
-		completed: true
-	},
-	{
-		title:"изучить react",
-		completed: false
-	}
-]
+
 
 @Component({
 	moduleId: module.id,
@@ -28,22 +13,10 @@ const todos: Todo[]=[
 })
 
 export class AppComponent{
-	title: string = 'angular 2do';
-	todos: Todo[]= todos;
-	newTodoTitle:string;
-
-	create(){
-		let todo: Todo = new Todo( this.newTodoTitle );
-		this.todos.push( todo );
-		// this.newTodoTitle ='';
-	}
-	toggle( todo: Todo ) {
-		todo.completed = !todo.completed 	
-	}
-	delete(todo: Todo){
-		let index = this.todos.indexOf( todo );
-		if ( index > -1 ){
-			this.todos.splice( index, 1 )
-		}
+	title: string = 'angular 2do';	
+	todos: Todo[] = todos;
+	create( title: string ){
+		const todo = new Todo( title);
+		this.todos.push(todo)
 	}
 }
---
+++ app/shared/data.ts
@@ -0,0 +1,14 @@
+export const todos=[
+	{
+		title:"изучить js",
+		completed: true
+	},
+	{
+		title:"изучить node",
+		completed: true
+	},
+	{
+		title:"изучить react",
+		completed: false
+	}
+]
